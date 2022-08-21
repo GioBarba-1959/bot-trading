2bb61c66c2da9aa315a7f5d2a8b57ce946108564
GioBarba-1959 <111189146+GioBarba-1959@users.noreply.github.com>
2022-08-21 19:20:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
+@app.route('/')
+def welcome():
+    return "<h1>Questo è il mio primo trading bot</h1>"
+
 @app.route("/hello")
 def index():
 	flash("Qual'è il tuo nome?")
